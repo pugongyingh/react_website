074f0dcf2e01c2c695641b4dcaa1c8a7e1ba9497
Derek Shiller <dcshiller@gmail.com>
2018-08-21 18:53:41 -0400

    update tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'],
+    'gatsby-plugin-sass'
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-36693042-1",
+      },
+    },
+  ],
+  ],
 }
