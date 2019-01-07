658570ea043fc9365cf90a09c7cfaed5aa827a97
Derek Shiller <dcshiller@gmail.com>
2019-01-06 20:22:31 -0500

    add future presentation

--- functions/file.js
+++ functions/file.js
@@ -1,11 +1,17 @@
 var fs = require("fs");
 
 exports.handler = function(event, context, callback) {
+
   fs.writeFile("/tmp/test.txt", "testing", function (err) {
     if (err) {
       context.fail("writeFile failed: " + err);
     } else {
       context.succeed("writeFile succeeded");
     }
+  });
+
+  callback(null, {
+    statusCode: 200,
+    body: fs.readFile("/tmp/test.txt"),
   });
 }
